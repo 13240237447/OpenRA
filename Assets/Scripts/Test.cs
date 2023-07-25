f27fdbbcc393156c6c6436d4f1afd33989b37b75
YangWenbo <shayilaixi@outlook.com>
2023-07-25 14:18:25 +0800

    新版行为树

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+
+namespace DefaultNamespace
+{
+    public class Test
+    {
+        private HashSet<int> nums = new HashSet<int>();
+        public void TestFun()
+        {
+            nums.Add(1);
+            
+        }
+    }
+}
